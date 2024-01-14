a47774f6da7040557d5d3fd093a14fd6de4dae34
Jaya priya <shahisaumya330@gmail.com>
2024-01-14 14:44:50 -0800

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,32 @@ function refreshWeather(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
-    
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
+    console.log(response.data.condition.description);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     cityElement.innerHTML = response.data.city;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
+    timeElement.innerHTML = formatDate(date);
+}
+function formatDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    let day = days[date.getDay()];
+
+    if (minutes<10){
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours} ${minutes}`;
 }
 function searchCity(city) {
     let apiKey = "b2a5adcct04b33178913oc335f405433";
